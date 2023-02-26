28c16fe3367ee26da284412cca6f21b4aac1023d
Maxim Khrypunov <khrypunovmax@gmail>
2023-02-26 10:47:20 +0200

    HW29

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 "use strict";
 // for(let i = 0; i < 3; i++)
-// {
+//  {
 //     setTimeout(()=> console.log(i));
-// }
+//  }
 var num;
-num = 12;
+num = "12";
 var str = "say my name";
 str = "lmn";
 var color;
@@ -24,27 +24,60 @@ function stringNumberAction(strNum) {
     }
     return res;
 }
-console.log(stringNumberAction("10"));
+console.log(stringNumberAction("2123"));
 var ar;
 ar = [1, 2, 3];
 var ar1;
 ar1 = [1, 2, "str"];
 var someColor;
-someColor = "blue";
+someColor = "yellow";
 var someComparator;
 someComparator = function (n1, n2) { return n1 - n2; };
 someComparator = f1;
 var person = { id: 12345678, name: "Maxim", age: 20 };
+// HOME WORK 29
 function shiftRound(str, shift) {
     if (shift === void 0) { shift = 1; }
-    //TODO
-    //
-    return "";
+    var res;
+    var arr = str.split('');
+    res = arr.map(function (element) {
+        var number = 0;
+        var askiNumber = element.charCodeAt(number);
+        if (askiNumber > 122 || askiNumber < 97) {
+            var newSymbol = String.fromCharCode(askiNumber);
+            return newSymbol;
+        }
+        else if (askiNumber < 122 && askiNumber > 97 && shift > 25) {
+            var newShift = shift - (Math.floor(shift / 25) * 25);
+            var counter = askiNumber + newShift;
+            if (counter > 122) {
+                counter = 97 + (counter - 123);
+                var newSymbol_1 = String.fromCharCode(counter);
+                return newSymbol_1;
+            }
+            var newSymbol = String.fromCharCode(counter);
+            return newSymbol;
+        }
+        else
+            (askiNumber < 122 && askiNumber > 97 && shift < 25);
+        {
+            var counter = askiNumber + shift;
+            if (counter > 122) {
+                counter = 97 + (counter - 123);
+                var newSymbol_2 = String.fromCharCode(counter);
+                return newSymbol_2;
+            }
+            var newSymbol = String.fromCharCode(counter);
+            return newSymbol;
+        }
+    });
+    return res.join("");
 }
-function unshiftRound(str, shift) {
-    if (shift === void 0) { shift = 1; }
-    //TODO
-    //
-    return "";
-}
+// function unshiftRound (str:string, shift:number = 1):string
+// {
+//     //TODO
+//     //
+//     return ""
+// }
+console.log(shiftRound("aabx!!", 4));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 // for(let i = 0; i < 3; i++)
-// {
+//  {
 //     setTimeout(()=> console.log(i));
-// }
-let num: number;
-num = 12;
+//  }
+let num: string;
+num = "12";
 let str: string = "say my name";
 str ="lmn";
 let color: "red" | "blue" | "green";
@@ -15,9 +15,9 @@ function f2(array: number[]):number
 {
 return array.reduce((res, cur) => res + cur)
 } 
-function stringNumberAction(strNum: string|number): number
+function stringNumberAction(strNum:string|number): number
 {
-    let res:number;
+let res:number;
 if (typeof strNum == "string")
 {
     res = strNum.length;
@@ -25,14 +25,14 @@ if (typeof strNum == "string")
 else { res = strNum*2;}
 return res;
 }
-console.log(stringNumberAction("10"));
+console.log (stringNumberAction("2123"))
 let ar: number[] | string [];
 ar = [1, 2, 3];
 let ar1 :(number|string)[];
 ar1 = [1, 2, "str"];
 type Color = "red"| "blue"| "green"| "yellow";
 let someColor: Color;
-someColor = "blue";
+someColor ="yellow"
 type Comparator = (num1: number, num2: number)=> number | ((array:number[]) => number);
 let someComparator: Comparator;
 someComparator = (n1, n2) => n1 - n2;
@@ -46,15 +46,51 @@ type Person =
 }
 const person: Person ={id:12345678 , name: "Maxim", age:20}
 
+// HOME WORK 29
+
 function shiftRound (str:string, shift:number = 1):string
 {
-    //TODO
-    //
-    return ""
-}
-function unshiftRound (str:string, shift:number = 1):string
+
+let res: string[];
+let arr = str.split('');
+res = arr.map((element)=>
 {
-    //TODO
-    //
-    return ""
+let number = 0;
+let askiNumber = element.charCodeAt(number);
+if (askiNumber>122 || askiNumber<97)
+{
+ let newSymbol = String.fromCharCode(askiNumber);
+ return  newSymbol;
 }
+else if (askiNumber<122 && askiNumber>97 && shift > 25) 
+{
+let newShift = shift - (Math.floor(shift/25)*25);
+let counter = askiNumber+newShift;
+if (counter>122)
+{counter = 97 + (counter-123);
+let newSymbol = String.fromCharCode(counter);
+return  newSymbol;}
+let newSymbol = String.fromCharCode(counter);
+return  newSymbol;
+}
+else(askiNumber<122 && askiNumber>97 && shift < 25);
+{let counter = askiNumber+shift;
+    if (counter>122)
+    {counter = 97 + (counter-123);
+    let newSymbol = String.fromCharCode(counter);
+    return  newSymbol;}
+    let newSymbol = String.fromCharCode(counter);
+    return  newSymbol}})
+return res.join("");
+}
+
+
+
+
+// function unshiftRound (str:string, shift:number = 1):string
+// {
+//     //TODO
+//     //
+//     return ""
+// }
+console.log(shiftRound("aabx!!", 4));
